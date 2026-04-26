8d60551b1355b99e34395df6f56d5c82ef388c77
THARUSHA <tharushadilmin123@gmail.com>
2026-04-26 18:31:50 +0530

    fix mobile navbar close

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.getElementById('menu_btn');
 const menu = document.getElementById('navbar');
 const menuCloseBtn = document.getElementById('menu-close');
+let navLinks = document.querySelectorAll('.menu_btn');
 
 
 menuBtn.addEventListener('click', () => {
@@ -9,4 +10,10 @@ menuBtn.addEventListener('click', () => {
 
 menuCloseBtn.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 });
