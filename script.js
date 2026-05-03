17375eea7d11d3417c55d9f0d5925b0c4a99f21b
THARUSHA <tharushadilmin123@gmail.com>
2026-05-03 17:15:51 +0530

    create login & resgister btn toggle

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 const menuBtn = document.getElementById('menu_btn');
 const menu = document.getElementById('navbar');
 const menuCloseBtn = document.getElementById('menu-close');
-let navLinks = document.querySelectorAll('.menu_btn');
+const navLinks = document.querySelectorAll('.menu_btn');
 
+if (menuBtn && menu) {
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+}
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
-
-menuCloseBtn.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
+if (menuCloseBtn && menu) {
+    menuCloseBtn.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+}
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        menu.classList.remove('active');
+        if (menu) {
+            menu.classList.remove('active');
+        }
     });
 });
+
+
+
+// Login/Register Form Toggle
+
+const loginButton = document.getElementById('LoginBtn');
+const registerButton = document.getElementById('RegisterBtn');
+const loginForm = document.getElementById('logIn_form');
+const registerForm = document.getElementById('register_form');
+
+loginButton.classList.add('active');
+
+function showLoginForm() {
+    loginForm.style.display = 'block';
+    registerForm.style.display = 'none';
+    loginButton.classList.add('active');
+    registerButton.classList.remove('active');
+}
+
+function showRegisterForm() {
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'block';
+    registerButton.classList.add('active');
+    loginButton.classList.remove('active');
+}
+
+loginButton.addEventListener('click', showLoginForm);
+registerButton.addEventListener('click', showRegisterForm);
+
