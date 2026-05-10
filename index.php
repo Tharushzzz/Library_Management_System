1f6128789f291010b3ece50d5d73dd38e530b723
GaganiTharusha2004 <gaganitharusha2004@gmail.com>
2026-05-11 01:06:56 +0530

    Add a table with headers

--- index.php
+++ index.php
@@ -398,6 +398,20 @@
               <div class="borrow_content_title">Book Borrows Details</div>
               <div class="borrow_content_title_line"></div>
             </div>
+            <div class="borrow_tab_head">
+              <table class="table borrow_table">
+                <thead class="borrow_table_head">
+                  <tr>
+                    <th scope="col">Borrow ID</th>
+                    <th scope="col">Book ID</th>
+                    <th scope="col">Member ID</th>
+                    <th scope="col">Borrow Status</th>
+                    <th scope="col">Borrow Date</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+              </table> 
+            </div>   
           </div>  
         </div>
 
