9124dc3b2d6c1d218c6b3df120439ac203ded75b
GaganiTharusha2004 <gaganitharusha2004@gmail.com>
2026-05-14 22:39:20 +0530

    Added handle delete book borrow

--- delete_data.php
+++ delete_data.php
@@ -132,7 +132,30 @@
     exit();
   }
 
-
+ // Handle delete book borrow 
+  if (isset($_GET['delete_borrow'])) {
+    $deleteBorrowID = $conn->real_escape_string($_GET['delete_borrow']);
+    $conn->query("DELETE FROM bookborrower WHERE borrow_id = '$deleteBorrowID'");
+    header("Location: index.php#v-pills-borrow");
+    $_SESSION['alert'] = '<script>
+                                const Toast = Swal.mixin({
+                                toast: true,
+                                position: "top-end",
+                                showConfirmButton: false,
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                    toast.onmouseenter = Swal.stopTimer;
+                                    toast.onmouseleave = Swal.resumeTimer;
+                                }
+                                });
+                                Toast.fire({
+                                icon: "success",
+                                title: "Book borrow deleted successfully"
+                                });
+                                </script>';
+    exit();
+  }
 
 
   // Handle delete fine
